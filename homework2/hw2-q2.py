dd954bf4726a77e0653478758db7c9a72e31a05d
martimfasantos <marty.f.a.santos@gmail.com>
2023-01-07 13:11:25 +0000

    homework2 start

---
+++ homework2/hw2-q2.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python
+
+# Deep Learning Homework 2
+
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import torchvision
+from matplotlib import pyplot as plt
+import numpy as np
+
+import utils
+
+class CNN(nn.Module):
+    
+    def __init__(self, dropout_prob):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=5)
+        self.conv1_activ = F.relu
+        self.conv1_max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3)
+        self.conv2_activ = F.relu
+        self.conv2_max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.fc1 = nn.Linear(320, 600) # TODO nao esta certo o input
+        self.fc1_drop = nn.Dropout2d(dropout_prob)
+
+        self.fc2 = nn.Linear(50, 120) # TODO nao esta certo o input
+        self.fc2_activ = F.relu
+
+        self.fc3 = nn.Linear(120, 8) # TODO nao esta certo o input
+        self.log_softmax = F.log_softmax()
+
+        
+    def forward(self, x):
+        """
+        x (batch_size x n_channels x height x width): a batch of training 
+        examples
+        """
+        print(x) # TODO dimensoes nao estao a bater certo
+
+        # Batch size = 8, images ?x? =>
+        #     x.shape = [8, 1, 28, 28]
+        x = self.conv1_activ(self.conv1_max_pool(self.conv1(x)))
+
+        x = self.conv2_activ(self.conv2_max_pool(self.conv2(x)))
+
+        # x = x.view(-1, 320)
+
+        x = self.fc1_drop(self.fc1(x))
+
+        x = self.fc2_activ(self.fc2(x))
+
+        x = self.fc3(x)
+        output = self.log_softmax(x)
+
+        return output
+
+def train_batch(X, y, model, optimizer, criterion, **kwargs):
+    """
+    X (n_examples x n_features)
+    y (n_examples): gold labels
+    model: a PyTorch defined model
+    optimizer: optimizer used in gradient step
+    criterion: loss function
+    """
+    # set the parameter gradients to zero
+    optimizer.zero_grad()
+    # Compute the model output
+    y_hat = model(X)
+    # Compute loss
+    loss = criterion(y_hat, y)
+    # Perform backpropagation
+    loss.backward()
+    # Update model weights
+    optimizer.step()
+
+    return loss.item()
+
+def predict(model, X):
+    """X (n_examples x n_features)"""
+    scores = model(X)  # (n_examples x n_classes)
+    predicted_labels = scores.argmax(dim=-1)  # (n_examples)
+    return predicted_labels
+
+
+def evaluate(model, X, y):
+    """
+    X (n_examples x n_features)
+    y (n_examples): gold labels
+    """
+    model.eval()
+    y_hat = predict(model, X)
+    n_correct = (y == y_hat).sum().item()
+    n_possible = float(y.shape[0])
+    model.train()
+    return n_correct / n_possible
+
+
+def plot(epochs, plottable, ylabel='', name=''):
+    plt.clf()
+    plt.xlabel('Epoch')
+    plt.ylabel(ylabel)
+    plt.plot(epochs, plottable)
+    plt.savefig('%s.pdf' % (name), bbox_inches='tight')
+
+
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+def plot_feature_maps(model, train_dataset):
+    
+    model.conv1.register_forward_hook(get_activation('conv1'))
+    
+    data, _ = train_dataset[4]
+    data.unsqueeze_(0)
+    output = model(data)
+
+    plt.imshow(data.reshape(28,-1)) 
+    plt.savefig('original_image.pdf')
+
+    k=0
+    act = activation['conv1'].squeeze()
+    fig,ax = plt.subplots(2,4,figsize=(12, 8))
+    
+    for i in range(act.size(0)//3):
+        for j in range(act.size(0)//2):
+            ax[i,j].imshow(act[k].detach().cpu().numpy())
+            k+=1  
+            plt.savefig('activation_maps.pdf') 
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-epochs', default=20, type=int,
+                        help="""Number of epochs to train for. You should not
+                        need to change this value for your plots.""")
+    parser.add_argument('-batch_size', default=8, type=int,
+                        help="Size of training batch.")
+    parser.add_argument('-learning_rate', type=float, default=0.01,
+                        help="""Learning rate for parameter updates""")
+    parser.add_argument('-l2_decay', type=float, default=0)
+    parser.add_argument('-dropout', type=float, default=0.3)
+    parser.add_argument('-optimizer',
+                        choices=['sgd', 'adam'], default='adam')
+    
+    opt = parser.parse_args()
+
+    utils.configure_seed(seed=42)
+
+    data = utils.load_classification_data()
+    dataset = utils.ClassificationDataset(data)
+    train_dataloader = DataLoader(
+        dataset, batch_size=opt.batch_size, shuffle=True)
+    dev_X, dev_y = dataset.dev_X, dataset.dev_y
+    test_X, test_y = dataset.test_X, dataset.test_y
+
+    # initialize the model
+    model = CNN(opt.dropout)
+    
+    # get an optimizer
+    optims = {"adam": torch.optim.Adam, "sgd": torch.optim.SGD}
+
+    optim_cls = optims[opt.optimizer]
+    optimizer = optim_cls(
+        model.parameters(), lr=opt.learning_rate, weight_decay=opt.l2_decay
+    )
+    
+    # get a loss criterion
+    criterion = nn.NLLLoss()
+    
+    # training loop
+    epochs = np.arange(1, opt.epochs + 1)
+    train_mean_losses = []
+    valid_accs = []
+    train_losses = []
+    for ii in epochs:
+        print('Training epoch {}'.format(ii))
+        for X_batch, y_batch in train_dataloader:
+            loss = train_batch(
+                X_batch, y_batch, model, optimizer, criterion)
+            train_losses.append(loss)
+
+        mean_loss = torch.tensor(train_losses).mean().item()
+        print('Training loss: %.4f' % (mean_loss))
+
+        train_mean_losses.append(mean_loss)
+        valid_accs.append(evaluate(model, dev_X, dev_y))
+        print('Valid acc: %.4f' % (valid_accs[-1]))
+
+    print('Final Test acc: %.4f' % (evaluate(model, test_X, test_y)))
+    # plot
+    config = "{}-{}-{}-{}".format(opt.learning_rate, opt.dropout, opt.l2_decay, opt.optimizer)
+
+    plot(epochs, train_mean_losses, ylabel='Loss', name='CNN-training-loss-{}'.format(config))
+    plot(epochs, valid_accs, ylabel='Accuracy', name='CNN-validation-accuracy-{}'.format(config))
+    
+    plot_feature_maps(model, dataset)
+
+if __name__ == '__main__':
+    main()
